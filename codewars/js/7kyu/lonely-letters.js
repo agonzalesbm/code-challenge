b7aae5773cc8ae0571c572a07468f23b907d7f4c
agonzalesbm <andres.gonzales.dev@gmail.com>
2026-05-22 20:54:53 -0400

    migration

---
+++ codewars/js/7kyu/lonely-letters.js
@@ -0,0 +1,12 @@
+/*
+ * https://www.codewars.com/kata/69cda5b85599f307742ce70a/train/javascript
+ * */
+
+function countLonelyLetters(text) {
+  const arr = text.split('');
+  const arra = arr.filter((c) => {
+    arr.some((e) => )
+  })
+  
+  return 0;
+}
